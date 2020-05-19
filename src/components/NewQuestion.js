00eab342495d986ba350e95323af0d6edee0bcb9
Abdelwahab1242 <Abdelwahab1242@gmail.com>
2020-05-19 21:57:59 +0200

    Connect the component to the redux store

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -1,8 +1,43 @@
 import React from "react";
 import { Segment, Header, Form, Divider } from "semantic-ui-react";
+import { connect } from "react-redux";
+import { handleSaveQuestion } from "../actions/questions";
+import { Redirect } from "react-router-dom";
 
 class NewQuestion extends React.Component {
+  state = {
+    validSubmit: false,
+    option1: "",
+    option2: "",
+  };
+
+  handleChange = (e) => {
+    this.setState({ [e.target.id]: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { authUser, handleSaveQuestion } = this.props;
+    const { option1, option2 } = this.state;
+
+    new Promise((res, rej) => {
+      handleSaveQuestion(option1, option2, authUser);
+      setTimeout(() => res("success"), 1000);
+    }).then(() => {
+      this.setState({
+        option1: "",
+        option2: "",
+      });
+      this.setState({ validSubmit: true });
+    });
+  };
+
   render() {
+    const disabled = this.state.option1 === "" || this.state.option2 === "";
+
+    if (this.state.validSubmit === true) {
+      return <Redirect to="/" />;
+    }
     return (
       <div style={{ margin: "2.5rem" }}>
         <Segment.Group>
@@ -13,16 +48,32 @@ class NewQuestion extends React.Component {
             attached="top"
             content="Create a New Question"
           />
-          <Form style={{ padding: "2.5rem" }}>
+          <Form style={{ padding: "2.5rem" }} onSubmit={this.handleSubmit}>
             <Header as="h2">Would You Rather?</Header>
             <Form.Field>
               <br />
-              <Form.Input placeholder="Enter First Option" />
+              <Form.Input
+                placeholder="Enter First Option"
+                id="option1"
+                value={this.state.option1}
+                onChange={this.handleChange}
+              />
               <Divider horizontal>Or</Divider>
-              <Form.Input placeholder="Enter Second Option" />
+              <Form.Input
+                placeholder="Enter Second Option"
+                id="option2"
+                value={this.state.option2}
+                onChange={this.handleChange}
+              />
             </Form.Field>
             <Form.Field>
-              <Form.Button size="medium" fluid positive content="Submit" />
+              <Form.Button
+                size="medium"
+                fluid
+                positive
+                content="Submit"
+                disabled={disabled}
+              />
             </Form.Field>
           </Form>
         </Segment.Group>
@@ -31,4 +82,10 @@ class NewQuestion extends React.Component {
   }
 }
 
-export default NewQuestion;
+const mapStateToProps = ({ authUser }) => {
+  return {
+    authUser,
+  };
+};
+
+export default connect(mapStateToProps, { handleSaveQuestion })(NewQuestion);
